732aa700326f5d737fd1f1d6965aa40915c0f352
longbui23 <builo@dickinson.edu>
2024-01-18 00:56:02 +0700

    query changed

--- SQL Files/Query Test.sql
+++ SQL Files/Query Test.sql
@@ -1,34 +1,58 @@
 -- Using Database
 USE Pet_Finder;
-	
--- Number of Vaccinated, Dewormed, Sterilized Pet
-SELECT 
-	COUNT(CASE WHEN V.Pet_Vaccinated = 'Yes' THEN F.PetID ELSE NULL END) AS num_vaccinated_pet,
-	COUNT(CASE WHEN D.Pet_Dewormed = 'Yes' THEN F.PetID ELSE NULL END) AS num_dewormed_pet,
-	COUNT(CASE WHEN S.Pet_Sterilized = 'Yes' THEN F.PetID ELSE NULL END) AS num_sterilized_pet
+
+SELECT * FROM Fact_Pet
+
+-- 1) Dogs vs Cats
+SELECT
+	T.Pet_Type,
+	COUNT(F.PetID ) AS Total_Case,
+	SUM(F.Quantity) AS Total_Number,
+	AVG(F.Fee)/AVG(F.Quantity) AS Average_Price,
+	MAX(F.age)/AVG(F.Quantity) AS maximum_age,
+	MIN(F.age)/AVG(F.Quantity) AS minimum_age,
+	ROUND(COUNT(CASE WHEN V.Pet_Vaccinated = 'Yes' THEN F.PetID ELSE NULL END) * 1.0/COUNT(F.PetID),2) AS pct_vaccinated,
+	ROUND(COUNT(CASE WHEN D.Pet_Dewormed = 'Yes' THEN F.PetID ELSE NULL END) * 1.0/COUNT(F.PetID),2) AS pct_dewormed,
+	ROUND(COUNT(CASE WHEN S.Pet_Sterilized = 'Yes' THEN F.PetID ELSE NULL END) * 1.0/COUNT(F.PetID),2) AS pct_sterilized
 FROM Fact_Pet F
 LEFT JOIN Dim_Vaccinated V ON F.Pet_Vaccinated_ID = V.Pet_Vaccinated_ID
 LEFT JOIN Dim_Dewormed D ON F.Pet_Dewormed_ID = D.Pet_Dewormed_ID
 LEFT JOIN Dim_Sterilized S ON F.Pet_Sterilized_ID = S.Pet_Sterlized_ID
+LEFT JOIN Dim_Type T ON T.Pet_Type_ID = F.Pet_Type_ID
+GROUP BY T.Pet_Type;
 
-SELECT * FROM Fact_Pet
 
--- 2 types of Pet
+-- 2) Different numbers of breed
 SELECT
-	t.Pet_Type,
-	COUNT(f.Pet_Type_ID ) AS Total_Case,
-	SUM(f.Quantity) AS Total_Number,
-	SUM(f.Fee) AS Total_Price
+	p1.Pet_Breed,
+	COUNT(DISTINCT f.PetID) AS num_case,
+	SUM(F.Quantity) AS Total_Number,
+	AVG(F.Fee)/AVG(F.Quantity) AS Avg_Price,
+	MAX(F.age)/AVG(F.Quantity) AS maximum_age,
+	MIN(F.age)/AVG(F.Quantity) AS minimum_age,
+	ROUND(COUNT(CASE WHEN V.Pet_Vaccinated = 'Yes' THEN F.PetID ELSE NULL END) * 1.0/COUNT(F.PetID),2) AS pct_vaccinated,
+	ROUND(COUNT(CASE WHEN D.Pet_Dewormed = 'Yes' THEN F.PetID ELSE NULL END) * 1.0/COUNT(F.PetID),2) AS pct_dewormed,
+	ROUND(COUNT(CASE WHEN S.Pet_Sterilized = 'Yes' THEN F.PetID ELSE NULL END) * 1.0/COUNT(F.PetID),2) AS pct_sterilized
 FROM Fact_Pet f
-LEFT JOIN Dim_Type t ON t.Pet_Type_ID = f.Pet_Type_ID
-GROUP BY t.Pet_Type;
+LEFT JOIN Dim_Breed p1 ON f.Pet_Breed1_ID = p1.Pet_Breed_ID
+LEFT JOIN Dim_Vaccinated V ON F.Pet_Vaccinated_ID = V.Pet_Vaccinated_ID
+LEFT JOIN Dim_Dewormed D ON F.Pet_Dewormed_ID = D.Pet_Dewormed_ID
+LEFT JOIN Dim_Sterilized S ON F.Pet_Sterilized_ID = S.Pet_Sterlized_ID
+GROUP BY p1.Pet_Breed;
 
 -- Types of FurLength
 SELECT
 	l.Pet_Furlength,
 	COUNT(f.Pet_Type_ID ) AS Total_Case,
 	SUM(f.Quantity) AS Total_Number,
-	SUM(f.Fee) AS Total_Price
+	SUM(f.Fee) AS Total_Price,
+	ROUND(COUNT(CASE WHEN V.Pet_Vaccinated = 'Yes' THEN F.PetID ELSE NULL END) * 1.0/COUNT(F.PetID),2) AS pct_vaccinated,
+	ROUND(COUNT(CASE WHEN D.Pet_Dewormed = 'Yes' THEN F.PetID ELSE NULL END) * 1.0/COUNT(F.PetID),2) AS pct_dewormed,
+	ROUND(COUNT(CASE WHEN S.Pet_Sterilized = 'Yes' THEN F.PetID ELSE NULL END) * 1.0/COUNT(F.PetID),2) AS pct_sterilized
 FROM Fact_Pet f
 LEFT JOIN Dim_FurLength l ON l.Pet_Furlength_ID = f.Pet_FurLength_ID
+LEFT JOIN Dim_Breed p1 ON f.Pet_Breed1_ID = p1.Pet_Breed_ID
+LEFT JOIN Dim_Vaccinated V ON F.Pet_Vaccinated_ID = V.Pet_Vaccinated_ID
+LEFT JOIN Dim_Dewormed D ON F.Pet_Dewormed_ID = D.Pet_Dewormed_ID
+LEFT JOIN Dim_Sterilized S ON F.Pet_Sterilized_ID = S.Pet_Sterlized_ID
 GROUP BY l.Pet_Furlength;
